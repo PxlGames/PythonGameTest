19795bcd3ddef7f29ed7118f4e1ad4dc0009fc9d
Pxl Dev <72934703+PxlGames@users.noreply.github.com>
2022-03-01 11:23:02 +0000

    Player and map Functions and classes

--- main.py
+++ main.py
@@ -1,1 +1,72 @@
-print("test")
+import os
+import map
+
+
+def ClearTerminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+isPlaying = True
+
+x_position = 5
+y_position = 1
+
+# Player class
+class Player:
+    def __init__(self):
+        self.Input()
+
+    def PlayerLoop(self):
+        self.Input()
+
+    def Input(self):
+        print("Input")
+        inputs = input()
+        
+        if inputs == "a" or inputs == "A":
+            self.Position(x_position - 1, y_position)
+
+        if inputs == "d" or inputs == "D":
+            self.Position(x_position + 1, y_position)
+
+        if inputs == "w" or inputs == "W":
+            self.Position(x_position, y_position + 1)
+
+        if inputs == "s" or inputs == "S":
+            self.Position(x_position, y_position - 1)
+
+        if inputs == "exit":
+            isPlaying = False
+            
+    def Position(self, _x_position, _y_position):
+        
+        print(x_position)
+        print(y_position)
+
+class DrawMapClass:    
+    def __init__(self):
+        self.DrawMap(1)
+
+    def MapLoop(self):
+        self.DrawMap(1)
+
+    def GetRow(self, area, row):
+        getRow = map.GetGlobalVariable("list_" + str(area) + "_" + str(row))
+        return getRow
+    
+    def DrawMap(self, area):
+        mapHeight = map.mapHeight
+        drawOrder = 1
+        list = ""
+    
+        while mapHeight > 0:
+            rowToDraw = self.GetRow(area, drawOrder)
+            print(list.join(rowToDraw))
+            mapHeight -= 1
+            drawOrder += 1
+
+player = Player()
+map = DrawMapClass()
+
+while isPlaying:
+    player.PlayerLoop()
+    map.__init__()
---
+++ map.py
@@ -0,0 +1,17 @@
+
+# Returns a global variable.
+def GetGlobalVariable(variableName):
+    return globals()[variableName]
+
+
+# Naming conventions:
+# Map height in rows.
+# List / area number / row number
+
+mapHeight = 6
+list_1_1 = ["█","█","█","█","█"]
+list_1_2 = ["█"," "," "," ","█"]
+list_1_3 = ["█"," ","█"," ","█"]
+list_1_4 = ["█"," ","█","█","█"]
+list_1_5 = ["█"," "," "," ","█"]
+list_1_6 = ["█","█","█","█","█"]
